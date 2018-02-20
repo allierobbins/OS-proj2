8a78b943e074f3164b70d5bb41d20546f08df6f1
allierobbins <allierobbins@users.noreply.github.com>
2018-02-19 22:58:53 -0600

    Update master.c

--- master.c
+++ master.c
@@ -10,136 +10,156 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 
-int consumerChecker(pid_t [], int, int);      // Prototypes
-void signalHandler();
-void killAll();
+int consumerStatus(pid_t [], int);      // Prototypes
+void endProgram();
+int detachAndRemove(int, void *);
 
-struct SharedMemory {     // Struct for shared memory
+struct SharedSpace {     // Struct for shared memory
 	int sharedVar;
 	int turn;
-	int flag[19];
-	int maxWrites;
+	int flag[];
 	int consumerProcesses;
 	int processes;
 };
 
 int shmid;        // Global variables
-struct SharedMemory *shmem;
-key_t key = 2018;
+struct SharedSpace *SM;
 
 int main(int argc, char* argv[]) {
 
-	signal(SIGINT, signalHandler);     // Signal handler
+	signal(SIGINT, endProgram);     // Signal handler
 
 	const char *PATH = "./consumer";
 
-	int op, totalProcesses,      // Local variables for index, max# of processes, timer,
-		index = 0,
-		maxWrites = 3,
-		consumerProcesses = 5,
-		timer = 100;
+	int totalProcesses = 0;      // Local variables for index, max# of processes, timer,
+	int	index = 0,
+	int	timer = 100;
 
-	if((shmid = shmget(key, sizeof(struct SharedMemory) * 2, IPC_CREAT | 0666)) < 0) {     // Createing shared memory ID
-		perror("shmget");
-		printf(stderr, "Error creating shraed memory - ending program.\n");     // If failed to create shared memory ID
-		exit(EXIT_FAILURE);
-	}
+    time_t startTime = time(NULL);      // Creating time variables to use timer
+    time_t currentTime = time(NULL);
 
-    if ((shmem = (struct SharedMemory *)shmat(shmid, NULL, 0)) == (struct SharedMemory *) -1) {     	// Attaching shared memory
-		perror("shmat");
-        printf(stderr, "Error attaching shraed memory - ending program.\n");      // If failed to attach shared memory
-        exit(EXIT_FAILURE);
+    pid_t consumer, producer, consumerList[consumerProcesses];     // Process IDs for children
+
+    if ((id = shmget(IPC_PRIVATE, sizeof(int), PERM)) == -1) {    // Create shared memory
+      perror ("Failed to create shared memory segment");      // Failed to create shared memory
+      return 1;
     }
 
-	time_t t1 = time(NULL);      // Creating time variables to use timer
-	time_t t2 = time(NULL);
+    if ((sharedtotal = (int *)shmat(id, NULL, 0)) == (void *)-1) {      // Attach self shared memory
+      perror("Failed to attach shared memory segment");       // Failed to attach self to shared memory
 
-	pid_t pid, consumers[consumerProcesses];     // Process IDs for child-consumers
+      if (shmctl(id, IPC_RMID, NULL) == -1)       // Detach shared memory
+        perror("Failed to remove memory segment");      // Failed to detach shared memory
+        return 1;
+    }
 
-	shmem->maxWrites = maxWrites;      // Writing to shared memory
-	shmem->sharedVar = 0;
-	shmem->consumerProcesses = consumerProcesses + 1;
-	shmem->turn = 0;
+	SM.sharedVar = 0;      // Writing to shared memory
+	SM.consumerProcesses =  5;
+	SM.turn = 0;
 
-	while((t2 - t1) < timer) {     // Timer to execute while under 100 seconds
+	do {     // Timer to execute while under 100 seconds
+
+    producer = fork();
+
+    if (producer == 0) {
+        execl("./producer", id, (char *)NULL);   //where code for producer goes
+        *sharedtotal = totalBytes;
+        return 1; }
+
 		if(totalProcesses < 20) {
 			if(index < consumerProcesses) {      // Fork 20 processes
 				pid = fork();
-				consumers[index] = pid;     // Keep record of child-consumer PIDs
+				consumerList[index] = pid;     // Keep record of child-consumer PIDs
 				totalProcesses++;     // Increace # of processes
 				index++;      // Increase index
 			}
 
-			if(pid == -1) {      // If error forking child-consumers
-				printf(stderr, "Error when forking consumers - ending program.\n");
-				killAll();      // End all processes & exit
-				exit(EXIT_FAILURE);
+			if(conusmer == -1 || producer == -1) {      // If error forking children
+				perror("Error when forking children.\n");
 
-			} else if(pid == 0) {      // Execute consumer code processes
+        if (detachAndRemove(id, sharedtotal) == -1) {     // Detach shared memory
+        perror("Error when detaching shared memory.\n");      // Error if failed to detach shared memory
+        return 1;
+        }
+				exit(1);
+
+			} else if (pid == 0) {      // Execute consumer code processes
 				char *id;
 				printf(id, "%i", index);      // Print PID and index
-				shmem->processes++;     // Write to shared memory - increase processes
+				Sm.processes++;     // Write to shared memory - increase processes
 
-				execl(PATH, id, (char *)NULL);
+				execl("./consumer", id, (char *)NULL);
 
-				_exit(EXIT_FAILURE);      // If consumer program execl fails - exit
+				exit(1);      // If consumer program execl fails - exit
 
 			}
 
-			t2 = time(NULL);     // Reset time
+			startTime = time(NULL);     // Reset time
 
 		} else {
-			printf(stderr, "There are too many processes running - killing all and ending program.\n");    // If lots of processes are running - exit
-			signalHandler(2);
-			exit(EXIT_FAILURE);
+			printf(stderr, "Too many processes running - killing all and ending program.\n");    // If lots of processes are running - exit
+			endProgram(2);
+			exit(1);
 		}
-
-		int check = consumerChecker(consumers, consumerProcesses, maxWrites);
-		if(check == 1) {      	// Check consumer processes status - If there's consumers running then continue - or let master finish
-			break;
-		}
+  } while((startTime - currentTime) < timer);
 
 	}
 
-	if((wait(NULL) > 0) && (shmem->processes != 0)) {      // If 100 seconds pass - check that consumers are running
-		printf(stderr,"Error in completing all processes in 100 seconds - Killing all processes.\n");      //If consumers running - kill all processes & error
-		signalHandler();
-	} else {     	//end successfully
-		killAll();
-		printf("Completed %i writes for %i processes.\n", maxWrites, consumerProcesses);
+	if((wait(NULL) >= 0) {      // 100 seconds have pass
+		perror("Error in completing all processes in 100 seconds.\n");      //If consumers running - kill all processes & error
+		endProgram();
+	} else {
+    if (detachAndRemove(id, sharedtotal) == -1) {     // Detach shared memory
+    perror("Failed to destroy shared memory segment");      // Error if failed ot detach shared memory
+    return 1;
+    }
+
+		printf("Finished %i logs for %i processes.\n", 20, consumerProcesses);
 	}
 }
 
-int consumerChecker(pid_t consumers[], int size, int maxWrites) {       // Checks if consumers are running
-	int c, status;     //variables
+int consumerStatus(pid_t consumerList[], int size) {       // Checks if consumers are running
+	int i, status;     //variables
 
-	for(c = 0; c < size; c++) {      //check PIDs of consumer process
-		pid_t wid;
-		wid = waitpid(consumers[c], &status, WNOHANG);
+	for(i = 0; i < size; i++) {      // Check PIDs status of consumer
+		pid_t wid = waitpid(consumerList[c], &status, WNOHANG);
 
 		if(wid != 0) {
-			consumers[c] = 0;
+			consumerList[i] = 0;
 		}
 	}
 
-	for(c = 0; c < size; c++) {
-		if(consumers[c] == 0)     //if process is running - continue
+	for(i = 0; i < size; i++) {
+		if(consumers[i] == 0)     // If process is running - continue
 			continue;
-		else      //process is done - return
+		else      // Process is done - return 0
 			return 0;
 	}
 		return 1;
 }
 
-void signalHandler() {      // Kills all when signal is received
+void endProgram() {      // Kills all when signal is received
     pid_t id = getpgrp();
 	printf("Signal received - exiting master program with PID: %i.\n", id);
-	killAll();     //kills all processes running
+
+  if (detachAndRemove(id, sharedVar) == -1) {
+      perror("Failed to destroy shared memory segment");
+      return 1;
+    }
+      //kills all processes running
     killpg(id, SIGINT);
     exit(EXIT_SUCCESS);
 }
 
-void killAll() {      // detaches all shared memory
-	shmctl(shmid, IPC_RMID, NULL);
-	shmdt(shmem);
+int detachAndRemove(int shmID, void *shmaddr) {
+  int error = 0;
+
+  if (shmdt(shmaddr) == -1)
+    error = errno;
+  if ((shmctl(shmID, IPC_RMID, NULL) == -1) && !error)
+    error = errno;
+  if (!error)
+    return 0;
+    errno = error;
+    return -1;
 }
